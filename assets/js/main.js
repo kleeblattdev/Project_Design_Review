80b4767824d8411e61cdd824983a16d60682cf17
karinlee <karinlee92@gmail.com>
2023-01-05 17:50:12 +0100

    added button toggle with js

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+/* First attempt at making the button change the color and BGC
+
+function getStarted(){
+document.getElementById("getStarted").style.color= "#fff";
+document.getElementById("getStarted").style.backgroundColor= "#27282A";
+} 
+
+*/
+
+console.log("I have no idea what I am doing :D")
+
+function getStarted(){
+    const clickMe = document.getElementById ('getStarted');
+    clickMe.classList.toggle('grayBG') //button should change the color and BGC
+}
+
+console.log("OMG it works, but what did I do?! LOLOLO")
